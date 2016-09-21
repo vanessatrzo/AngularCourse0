cc4d4db42a87da5e23c49afbc4623239fc100316
vanessatrzo <lv.trevizo@gmail.com>
2016-09-20 23:09:11 -0600

    Final commit

--- www/js/app.js
+++ www/js/app.js
@@ -1,85 +1,103 @@
-// Ionic Starter App
+(function(){
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+  var gem = {
+    name: 'Azurite',
+    price: '2.95',
+    description: 'Lorem ipsum dolor sit amet',
+    canPurchase: false,
+    soldOut: true
+  };
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  var gems = [
+    {
+      name: 'Azurite',
+      price: '2.95',
+      description: 'Lorem ipsum dolor sit amet',
+      canPurchase: false,
+      soldOut: true,
+      images: [
+        {id:0, url:'img/gem-01.gif'},
+        {id:1, url:'img/gem-02.gif'},
+        {id:2, url:'img/gem-03.gif'}
+      ],
+      reviews: [
+        {stars: 5, body: 'I love this product', author:'lv.trevizo@gmail.com'},
+        {stars: 5, body: 'I love this product', author:'lv.trevizo@gmail.com'}
+      ]
+    },
+    {
+      name: 'Bloodstone',
+      price: '5.95',
+      description: 'Lorem ipsum dolor sit amet',
+      canPurchase: false,
+      soldOut: true,
+      images: [
+        {id:0, url:'img/gem-04.gif'},
+        {id:1, url:'img/gem-05.gif'},
+        {id:2, url:'img/gem-06.gif'}
+      ],
+      reviews: [
+        {stars: 5, body: 'I love this product', author:'lv.trevizo@gmail.com'},
+        {stars: 5, body: 'I love this product', author:'lv.trevizo@gmail.com'}
+      ]
+    },
+    {
+      name: 'Zircon',
+      price: '3.95',
+      description: 'Lorem ipsum dolor sit amet',
+      canPurchase: false,
+      soldOut: true,
+      images: [
+        {id:0, url:'img/gem-07.gif'},
+        {id:1, url:'img/gem-08.gif'},
+        {id:2, url:'img/gem-09.gif'}
+      ],
+      reviews: [
+        {stars: 5, body: 'I love this product', author:'lv.trevizo@gmail.com'},
+        {stars: 5, body: 'I love this product', author:'lv.trevizo@gmail.com'}
+      ]
+    } 
+  ];
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+  var app = angular.module('gemStore', ['store-products']);
 
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
+  app.controller('StoreController', function(){
+    this.products = gems;
   });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  app.controller('PanelController', function(){
+    this.tab = 1;
 
-});
+    this.selectTab = function(setTab){
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    };
+  });
+
+  app.controller('ReviewsController',function(){
+      this.review = {};
+      this.addReview = function(product){
+        product.reviews.push(this.review);
+        this.review = {};
+      }
+  });
+  
+  app.controller('GalleryController', function(){
+    this.current = 0;
+    this.setCurrent = function(imageNumber){
+      this.current = imageNumber || 0;
+    }
+  })
+
+  /*app.directive('productTitle', function(){
+    return{
+      restrict: 'E',
+      templateUrl: 'product-title.html'
+    };
+  });
+*/
+
+})();
